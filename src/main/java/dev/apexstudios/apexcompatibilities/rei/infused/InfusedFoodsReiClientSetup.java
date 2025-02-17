9ac627ce1c2d9aab6ab2e9684112a8dfce1a09ce
Apex <robsonlawson3@gmail.com>
2025-02-17 20:01:37 +0000

    Match latest changes in InfusedFoods

--- src/main/java/dev/apexstudios/apexcompatibilities/rei/infused/InfusedFoodsReiClientSetup.java
+++ src/main/java/dev/apexstudios/apexcompatibilities/rei/infused/InfusedFoodsReiClientSetup.java
@@ -1,7 +1,6 @@
 package dev.apexstudios.apexcompatibilities.rei.infused;
 
 import dev.apexstudios.infusedfoods.InfusedFoods;
-import dev.apexstudios.infusedfoods.fluid.PotionFluidSetup;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.entry.CollapsibleEntryRegistry;
 import me.shedaniel.rei.api.common.entry.type.VanillaEntryTypes;
@@ -11,13 +10,6 @@ import net.minecraft.network.chat.Component;
 public final class InfusedFoodsReiClientSetup implements REIClientPlugin {
     @Override
     public void registerCollapsibleEntries(CollapsibleEntryRegistry registry) {
-        registry.group(
-                InfusedFoods.identifier("rei_group.bucket"),
-                Component.literal("Potion Buckets"),
-                VanillaEntryTypes.ITEM,
-                stack -> stack.getValue().is(PotionFluidSetup.BUCKET)
-        );
-
         registry.group(
                 InfusedFoods.identifier("rei_group.infused"),
                 Component.literal("Infused Food"),
