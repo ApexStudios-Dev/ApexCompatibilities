ee8df2176b9e99c78e5711b05ce080b4d16a4896
Apex <robsonlawson3@gmail.com>
2025-02-19 13:48:12 +0000

    Yeet old unneeded `@Mod` entry point

--- src/main/java/dev/apexstudios/apexcompatibilities/ApexCompatibilities.java
+++
@@ -1,8 +0,0 @@
-package dev.apexstudios.apexcompatibilities;
-
-import net.neoforged.fml.common.Mod;
-
-@Mod(ApexCompatibilities.ID)
-public final class ApexCompatibilities {
-    public static final String ID = "apexcompatibilities";
-}
