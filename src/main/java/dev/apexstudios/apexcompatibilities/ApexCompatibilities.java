bef5822fc18b428fca092dec7b81897e4bd0d757
Apex <robsonlawson3@gmail.com>
2025-02-23 11:22:46 +0000

    Refactor to use new compat manager system

---
+++ src/main/java/dev/apexstudios/apexcompatibilities/ApexCompatibilities.java
@@ -0,0 +1,15 @@
+package dev.apexstudios.apexcompatibilities;
+
+import dev.apexstudios.apexcore.core.ApexCore;
+
+public interface ApexCompatibilities {
+    String ID = "apexcompatibilities";
+
+    String APEXCORE = ApexCore.ID;
+    String ITEM_RESISTANCE = "itemresistance";
+    String INFUSED_FOODS = "infusedfoods";
+    String FANTASY_DICE = "fantasydice";
+    String FANTASY_FURNITURE = "fantasyfurniture";
+    String FANTASY_FURNITURE_NORDIC = FANTASY_FURNITURE + "_nordic";
+    String FANTASY_FURNITURE_VENTHYR = FANTASY_FURNITURE + "_venthyr";
+}
--- src/main/java/dev/apexstudios/apexcompatibilities/rei/ReiServerSetup.java
+++ src/main/java/dev/apexstudios/apexcompatibilities/rei/ReiServerSetup.java
@@ -1,12 +1,10 @@
 package dev.apexstudios.apexcompatibilities.rei;
 
-import com.google.common.base.Suppliers;
+import dev.apexstudios.apexcompatibilities.ApexCompatibilities;
+import dev.apexstudios.apexcompatibilities.CompatManager;
 import dev.apexstudios.apexcompatibilities.rei.dice.FantasyDiceReiServerSetup;
 import dev.apexstudios.apexcompatibilities.rei.furniture.FantasyFurnitureReiServerSetup;
 import dev.apexstudios.apexcompatibilities.rei.infused.InfusedFoodsReiServerSetup;
-import java.util.Map;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
 import me.shedaniel.rei.api.common.display.DisplaySerializerRegistry;
 import me.shedaniel.rei.api.common.entry.comparison.FluidComparatorRegistry;
 import me.shedaniel.rei.api.common.entry.comparison.ItemComparatorRegistry;
@@ -14,67 +12,56 @@ import me.shedaniel.rei.api.common.entry.settings.EntrySettingsAdapterRegistry;
 import me.shedaniel.rei.api.common.entry.type.EntryTypeRegistry;
 import me.shedaniel.rei.api.common.fluid.FluidSupportProvider;
 import me.shedaniel.rei.api.common.plugins.REICommonPlugin;
-import me.shedaniel.rei.api.common.plugins.REIPlugin;
 import me.shedaniel.rei.api.common.registry.display.ServerDisplayRegistry;
 import me.shedaniel.rei.api.common.transfer.info.stack.SlotAccessorRegistry;
 import me.shedaniel.rei.forge.REIPluginCommon;
-import net.neoforged.fml.ModList;
 
 @REIPluginCommon
 public final class ReiServerSetup implements REICommonPlugin {
-    private static final Map<String, Supplier<? extends REICommonPlugin>> MODS = Map.of(
-            "infusedfoods", Suppliers.memoize(InfusedFoodsReiServerSetup::new),
-            "fantasydice", Suppliers.memoize(FantasyDiceReiServerSetup::new),
-            "fantasyfurniture", Suppliers.memoize(FantasyFurnitureReiServerSetup::new)
-            // "fantasyfurniture_nordic", Suppliers.memoize(NordicFurnitureSetReiSetup::new)
+    private final CompatManager<REICommonPlugin> manager = CompatManager.create(REICommonPlugin.class, builder -> builder
+                    .with(ApexCompatibilities.INFUSED_FOODS, InfusedFoodsReiServerSetup::new)
+                    .with(ApexCompatibilities.FANTASY_DICE, FantasyDiceReiServerSetup::new)
+                    .with(ApexCompatibilities.FANTASY_FURNITURE, FantasyFurnitureReiServerSetup::new)
+                    // .with(ApexCompatibilities.FANTASY_FURNITURE_NORDIC, NordicFurnitureSetReiSetup::new)
     );
 
     @Override
     public void registerEntryTypes(EntryTypeRegistry registry) {
-        setupMod(MODS, setup -> setup.registerEntryTypes(registry));
+        manager.forEach(compat -> compat.registerEntryTypes(registry));
     }
 
     @Override
     public void registerEntrySettingsAdapters(EntrySettingsAdapterRegistry registry) {
-        setupMod(MODS, setup -> setup.registerEntrySettingsAdapters(registry));
+        manager.forEach(compat -> compat.registerEntrySettingsAdapters(registry));
     }
 
     @Override
     public void registerItemComparators(ItemComparatorRegistry registry) {
-        setupMod(MODS, setup -> setup.registerItemComparators(registry));
+        manager.forEach(compat -> compat.registerItemComparators(registry));
     }
 
     @Override
     public void registerFluidComparators(FluidComparatorRegistry registry) {
-        setupMod(MODS, setup -> setup.registerFluidComparators(registry));
+        manager.forEach(compat -> compat.registerFluidComparators(registry));
     }
 
     @Override
     public void registerFluidSupport(FluidSupportProvider support) {
-        setupMod(MODS, setup -> setup.registerFluidSupport(support));
+        manager.forEach(compat -> compat.registerFluidSupport(support));
     }
 
     @Override
     public void registerDisplays(ServerDisplayRegistry registry) {
-        setupMod(MODS, setup -> setup.registerDisplays(registry));
+        manager.forEach(compat -> compat.registerDisplays(registry));
     }
 
     @Override
     public void registerDisplaySerializer(DisplaySerializerRegistry registry) {
-        setupMod(MODS, setup -> setup.registerDisplaySerializer(registry));
+        manager.forEach(compat -> compat.registerDisplaySerializer(registry));
     }
 
     @Override
     public void registerSlotAccessors(SlotAccessorRegistry registry) {
-        setupMod(MODS, setup -> setup.registerSlotAccessors(registry));
-    }
-
-    static <TSetup extends REIPlugin<?>> void setupMod(Map<String, Supplier<? extends TSetup>> map, Consumer<? super TSetup> consumer) {
-        var modList = ModList.get();
-
-        map.forEach((modId, supplier) -> {
-            if(modList.isLoaded(modId))
-                consumer.accept(supplier.get());
-        });
+        manager.forEach(compat -> compat.registerSlotAccessors(registry));
     }
 }
