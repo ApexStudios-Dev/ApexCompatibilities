a6fe8f467377d49673687c1b5a718f5769378949
Apex <robsonlawson3@gmail.com>
2025-03-03 09:38:25 +0000

    Bump ApexCore version

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,22 +20,22 @@ repositories {
 }
 
 dependencies {
-    includeMod(libs.apexcore, false, true, true)
-    includeMod(libs.itemresistance, true, false, false)
-    includeMod(libs.infusedfoods, true, false, false)
-    includeMod(libs.fantasydice, true, false, false)
+    includeMod(libs.apexcore, false, true)
+    includeMod(libs.itemresistance, true, false)
+    includeMod(libs.infusedfoods, true, false)
+    includeMod(libs.fantasydice, true, false)
 
-    includeMod(libs.fantasyfurniture.asProvider(), true, true, false)
-    includeMod(libs.fantasyfurniture.nordic, true, false, false)
-    includeMod(libs.fantasyfurniture.venthyr, true, false, false)
-    includeMod(libs.fantasyfurniture.bone, true, false, false)
-    includeMod(libs.fantasyfurniture.dunmer, true, false, false)
+    includeMod(libs.fantasyfurniture.asProvider(), true, true)
+    includeMod(libs.fantasyfurniture.nordic, true, false)
+    includeMod(libs.fantasyfurniture.venthyr, true, false)
+    includeMod(libs.fantasyfurniture.bone, true, false)
+    includeMod(libs.fantasyfurniture.dunmer, true, false)
 
     implementation(libs.rei)
     compileOnly(libs.rei.api)
 }
 
-fun includeMod(mod: Provider<MinimalExternalModuleDependency>, compile: Boolean = false, at: Boolean = false, injection: Boolean = false) {
+fun includeMod(mod: Provider<MinimalExternalModuleDependency>, compile: Boolean = false, at: Boolean = false) {
     dependencies {
         if(compile)
             compileOnly(mod)
@@ -46,7 +46,5 @@ fun includeMod(mod: Provider<MinimalExternalModuleDependency>, compile: Boolean 
 
         if(at)
             accessTransformers(mod)
-        if(injection)
-            interfaceInjectionData(mod)
     }
 }
