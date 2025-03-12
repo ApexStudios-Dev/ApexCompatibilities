7f4f181ba5d01126e6f250074bd0fdf9b00d181a
Apex <29412632+ApexModder@users.noreply.github.com>
2025-03-12 03:36:12 +0000

    Add royal furniture set support (#13)

--- src/main/java/dev/apexstudios/apexcompatibilities/ApexCompatibilities.java
+++ src/main/java/dev/apexstudios/apexcompatibilities/ApexCompatibilities.java
@@ -15,4 +15,5 @@ public interface ApexCompatibilities {
     String FANTASY_FURNITURE_BONE = FANTASY_FURNITURE + "_bone";
     String FANTASY_FURNITURE_DUNMER = FANTASY_FURNITURE + "_dunmer";
     String FANTASY_FURNITURE_NECROLORD = FANTASY_FURNITURE + "_necrolord";
+    String FANTASY_FURNITURE_ROYAL = FANTASY_FURNITURE + "_royal";
 }
---
+++ src/main/java/dev/apexstudios/apexcompatibilities/rei/furniture/RoyalFurnitureSetReiClientSetup.java
@@ -0,0 +1,9 @@
+package dev.apexstudios.apexcompatibilities.rei.furniture;
+
+import dev.apexstudios.fantasyfurniture.royal.RoyalFurnitureSet;
+
+public final class RoyalFurnitureSetReiClientSetup extends FurnitureSetReiClientSetup {
+    public RoyalFurnitureSetReiClientSetup() {
+        super(RoyalFurnitureSet.FURNITURE_SET);
+    }
+}
