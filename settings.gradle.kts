8b6775f861866843bb15d144188b130e4a985465
Apex <robsonlawson3@gmail.com>
2025-04-11 17:38:33 +0100

    Bump NeoForge, ApexCore and REI versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        library("apexcore", "dev.apexstudios", "apexcore").version("21.5.2")
+        library("apexcore", "dev.apexstudios", "apexcore").version("21.5.3")
         library("itemresistance", "dev.apexstudios", "itemresistance").version("21.5.1")
         library("infusedfoods", "dev.apexstudios", "infusedfoods").version("21.5.1")
         library("fantasydice", "dev.apexstudios", "fantasydice").version("21.5.1")
@@ -30,7 +30,7 @@ dependencyResolutionManagement {
         library("fantasyfurniture_necrolord", "dev.apexstudios", "fantasyfurniture-necrolord").version("21.5.1")
         library("fantasyfurniture_royal", "dev.apexstudios", "fantasyfurniture-royal").version("21.5.1")
 
-        version("rei", "18.0.800")
+        version("rei", "19.0.805")
         library("rei", "me.shedaniel", "RoughlyEnoughItems-neoforge").versionRef("rei")
         library("rei-api", "me.shedaniel", "RoughlyEnoughItems-api-neoforge").versionRef("rei")
         library("rei-default-plugin", "me.shedaniel", "RoughlyEnoughItems-default-plugin-neoforge").versionRef("rei")
