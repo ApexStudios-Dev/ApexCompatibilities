e090756fedd122ac06f0be3d3649eec3b3965cbb
Apex <29412632+ApexModder@users.noreply.github.com>
2025-06-04 17:55:39 +0100

    Update to match FF changes (#29)

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
     maven("https://maven.architectury.dev")
     maven("https://maven.blamejared.com")
     maven("https://modmaven.dev")
+    maven("https://maven.apexstudios.dev/private")
 
     maven("https://api.modrinth.com/maven") {
         content {
