2b5b9dd71c7765008079976d0ac3141e19a6bd88
Apex <29412632+ApexModder@users.noreply.github.com>
2025-07-08 19:44:19 +0100

    [21.5] Merge development changes (#31)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import dev.apexstudios.gradle.single.ApexSingleExtension
 
 plugins {
-    id("apex-conventions.neoforge")
-    id("apex-conventions.immaculate")
-    id("apex-conventions.maven-publishing")
+    id("apex-conventions.neoforge") version "0.1.69"
+    id("apex-conventions.immaculate") version "0.1.69"
+    id("apex-conventions.maven-publishing") version "0.1.69"
 }
 
 group = "dev.apexstudios"
@@ -13,20 +13,6 @@ apex.extendCompilerErrors()
 
 val single = ApexSingleExtension.getOrCreate(project)
 single.withDataGen()
-
-repositories {
-    maven("https://maven.shedaniel.me")
-    maven("https://maven.architectury.dev")
-    maven("https://maven.blamejared.com")
-    maven("https://modmaven.dev")
-    maven("https://maven.apexstudios.dev/private")
-
-    maven("https://api.modrinth.com/maven") {
-        content {
-            includeGroup("maven.modrinth")
-        }
-    }
-}
 
 dependencies {
     includeMod(libs.apexcore, false, true)
