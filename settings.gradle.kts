8c3c0af648624f849616fb46366047b6235920f3
Apex <robsonlawson3@gmail.com>
2025-07-08 20:24:23 +0100

    Initial port to 21.7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        library("apexcore", "dev.apexstudios", "apexcore").version("21.6.0")
+        library("apexcore", "dev.apexstudios", "apexcore").version("21.6.5-beta-pr-47")
         library("itemresistance", "dev.apexstudios", "itemresistance").version("21.6.0")
         library("infusedfoods", "dev.apexstudios", "infusedfoods").version("21.6.0")
         library("fantasydice", "dev.apexstudios", "fantasydice").version("21.6.0")
