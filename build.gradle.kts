09b49f9e18b2c13f8fc04cea5002ecae60fb31e8
Apex <robsonlawson3@gmail.com>
2026-01-03 07:14:18 +0000

    Setup formatting checks

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,9 @@ import dev.apexstudios.gradle.ApexExtension
 import dev.apexstudios.gradle.single.ApexSingleExtension
 
 plugins {
-    id("apex-conventions.neoforge") version "0.1.87"
-    id("apex-conventions.maven-publishing") version "0.1.87"
+    id("apex-conventions.neoforge") version "0.1.88"
+    id("apex-conventions.maven-publishing") version "0.1.88"
+    id("apex-conventions.jspecify") version "0.1.88"
 }
 
 group = "dev.apexstudios"
