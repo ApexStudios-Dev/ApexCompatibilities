222a31c2663e0abfd93962267ccf8b4fbb4d3e6c
Apex <29412632+ApexModder@users.noreply.github.com>
2026-05-06 23:50:33 +0100

    [26.1] Merge dev into main (#59)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,25 +19,25 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        version("neoforge", "26.1.0.18-beta")
+        version("neoforge", "26.1.2.42-beta")
 
         library("registree", "dev.apexstudios", "registree").version("26.1.0")
-        library("apexcore", "dev.apexstudios", "apexcore").version("26.1.1")
+        library("apexcore", "dev.apexstudios", "apexcore").version("26.1.2")
         bundle("apexcore", listOf("registree", "apexcore"))
 
         library("itemresistance", "dev.apexstudios", "itemresistance").version("26.1.2")
         library("infusedfoods", "dev.apexstudios", "infusedfoods").version("26.1.2")
-        library("fantasydice", "dev.apexstudios", "fantasydice").version("26.1.2")
+        library("fantasydice", "dev.apexstudios", "fantasydice").version("26.1.4")
 
-        version("fantasyfurniture", "26.1.4")
+        version("fantasyfurniture", "26.1.7")
         library("fantasyfurniture", "dev.apexstudios", "fantasyfurniture").versionRef("fantasyfurniture")
-        library("fantasyfurniture_nordic", "dev.apexstudios", "nordic").versionRef("fantasyfurniture")
-        library("fantasyfurniture_venthyr", "dev.apexstudios", "venthyr").versionRef("fantasyfurniture")
-        library("fantasyfurniture_bone", "dev.apexstudios", "bone").versionRef("fantasyfurniture")
-        library("fantasyfurniture_dunmer", "dev.apexstudios", "dunmer").versionRef("fantasyfurniture")
-        library("fantasyfurniture_necrolord", "dev.apexstudios", "necrolord").versionRef("fantasyfurniture")
-        library("fantasyfurniture_royal", "dev.apexstudios", "royal").versionRef("fantasyfurniture")
-        library("fantasyfurniture_decorations", "dev.apexstudios", "decorations").versionRef("fantasyfurniture")
+        library("fantasyfurniture_nordic", "dev.apexstudios", "fantasyfurniture_nordic").versionRef("fantasyfurniture")
+        library("fantasyfurniture_venthyr", "dev.apexstudios", "fantasyfurniture_venthyr").versionRef("fantasyfurniture")
+        library("fantasyfurniture_bone", "dev.apexstudios", "fantasyfurniture_bone").versionRef("fantasyfurniture")
+        library("fantasyfurniture_dunmer", "dev.apexstudios", "fantasyfurniture_dunmer").versionRef("fantasyfurniture")
+        library("fantasyfurniture_necrolord", "dev.apexstudios", "fantasyfurniture_necrolord").versionRef("fantasyfurniture")
+        library("fantasyfurniture_royal", "dev.apexstudios", "fantasyfurniture_royal").versionRef("fantasyfurniture")
+        library("fantasyfurniture_decorations", "dev.apexstudios", "fantasyfurniture_decorations").versionRef("fantasyfurniture")
 
         version("rei", "21.9.813")
         library("rei", "me.shedaniel", "RoughlyEnoughItems-neoforge").versionRef("rei")
@@ -45,9 +45,9 @@ dependencyResolutionManagement {
         library("rei-default-plugins", "me.shedaniel", "RoughlyEnoughItems-default-plugin-neoforge").versionRef("rei")
         bundle("rei", listOf("rei-api", "rei-default-plugins"))
 
-        version("jei", "29.2.0.20")
-        library("jei-api", "mezz.jei", "jei-26.1-neoforge-api").versionRef("jei")
-        library("jei", "mezz.jei", "jei-26.1-neoforge").versionRef("jei")
+        version("jei", "29.5.0.28")
+        library("jei-api", "mezz.jei", "jei-26.1.2-neoforge-api").versionRef("jei")
+        library("jei", "mezz.jei", "jei-26.1.2-neoforge").versionRef("jei")
 
         library("jade", "maven.modrinth", "jade").version("26.0.6+neoforge")
     }
@@ -81,13 +81,13 @@ if(file("../../FantasyFurniture/26.1").exists()) {
 
         dependencySubstitution {
             substitute(module("dev.apexstudios:fantasyfurniture")).using(project(":"))
-            substitute(module("dev.apexstudios:nordic")).using(project(":nordic"))
-            substitute(module("dev.apexstudios:venthyr")).using(project(":venthyr"))
-            substitute(module("dev.apexstudios:bone")).using(project(":bone"))
-            substitute(module("dev.apexstudios:dunmer")).using(project(":dunmer"))
-            substitute(module("dev.apexstudios:necrolord")).using(project(":necrolord"))
-            substitute(module("dev.apexstudios:royal")).using(project(":royal"))
-            substitute(module("dev.apexstudios:decorations")).using(project(":decorations"))
+            substitute(module("dev.apexstudios:fantasyfurniture_nordic")).using(project(":fantasyfurniture_nordic"))
+            substitute(module("dev.apexstudios:fantasyfurniture_venthyr")).using(project(":fantasyfurniture_venthyr"))
+            substitute(module("dev.apexstudios:fantasyfurniture_bone")).using(project(":fantasyfurniture_bone"))
+            substitute(module("dev.apexstudios:fantasyfurniture_dunmer")).using(project(":fantasyfurniture_dunmer"))
+            substitute(module("dev.apexstudios:fantasyfurniture_necrolord")).using(project(":fantasyfurniture_necrolord"))
+            substitute(module("dev.apexstudios:fantasyfurniture_royal")).using(project(":fantasyfurniture_royal"))
+            substitute(module("dev.apexstudios:fantasyfurniture_decorations")).using(project(":fantasyfurniture_decorations"))
         }
     }
 }
