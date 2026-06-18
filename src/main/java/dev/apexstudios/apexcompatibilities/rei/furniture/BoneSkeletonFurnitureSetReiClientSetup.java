3a8ed712f339178ff72463e3198491b2ff2f0b14
Apex <29412632+ApexModder@users.noreply.github.com>
2026-06-18 21:29:01 +0100

    [26.2] Reenable REI support (#64)

--- src/main/java/dev/apexstudios/apexcompatibilities/rei/furniture/BoneSkeletonFurnitureSetReiClientSetup.java
+++ src/main/java/dev/apexstudios/apexcompatibilities/rei/furniture/BoneSkeletonFurnitureSetReiClientSetup.java
@@ -1,9 +1,9 @@
-//package dev.apexstudios.apexcompatibilities.rei.furniture;
-//
-//import dev.apexstudios.fantasyfurniture.bone.common.SkeletonFurnitureSet;
-//
-//public final class BoneSkeletonFurnitureSetReiClientSetup extends FurnitureSetReiClientSetup {
-//    public BoneSkeletonFurnitureSetReiClientSetup() {
-//        super(SkeletonFurnitureSet.FURNITURE_SET.registree, "Skeleton");
-//    }
-//}
+package dev.apexstudios.apexcompatibilities.rei.furniture;
+
+import dev.apexstudios.fantasyfurniture.bone.common.SkeletonFurnitureSet;
+
+public final class BoneSkeletonFurnitureSetReiClientSetup extends FurnitureSetReiClientSetup {
+    public BoneSkeletonFurnitureSetReiClientSetup() {
+        super(SkeletonFurnitureSet.FURNITURE_SET.registree, "Skeleton");
+    }
+}
--- src/main/java/dev/apexstudios/apexcompatibilities/rei/furniture/DunmerFurnitureSetReiClientSetup.java
+++ src/main/java/dev/apexstudios/apexcompatibilities/rei/furniture/DunmerFurnitureSetReiClientSetup.java
@@ -1,9 +1,9 @@
-//package dev.apexstudios.apexcompatibilities.rei.furniture;
-//
-//import dev.apexstudios.fantasyfurniture.dunmer.common.DunmerFurnitureSet;
-//
-//public final class DunmerFurnitureSetReiClientSetup extends FurnitureSetReiClientSetup {
-//    public DunmerFurnitureSetReiClientSetup() {
-//        super(DunmerFurnitureSet.REGISTREE, "Dunmer");
-//    }
-//}
+package dev.apexstudios.apexcompatibilities.rei.furniture;
+
+import dev.apexstudios.fantasyfurniture.dunmer.common.DunmerFurnitureSet;
+
+public final class DunmerFurnitureSetReiClientSetup extends FurnitureSetReiClientSetup {
+    public DunmerFurnitureSetReiClientSetup() {
+        super(DunmerFurnitureSet.REGISTREE, "Dunmer");
+    }
+}
